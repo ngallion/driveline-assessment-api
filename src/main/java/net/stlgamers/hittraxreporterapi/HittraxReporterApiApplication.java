2e8a22d4680c7ad301a99d81314b155ad50ce7e6
ngallion <tndg1@unigroupinc.com>
2018-11-30 22:04:40 -0600

    initial commit

---
+++ src/main/java/net/stlgamers/hittraxreporterapi/HittraxReporterApiApplication.java
@@ -0,0 +1,12 @@
+package net.stlgamers.hittraxreporterapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HittraxReporterApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HittraxReporterApiApplication.class, args);
+	}
+}
