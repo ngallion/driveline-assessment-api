09557cafd3de2058e98855340cd7334f23f8348c
ngallion <tndg1@unigroupinc.com>
2018-12-01 19:24:13 -0600

    removed unused dependency

--- src/main/java/net/stlgamers/hittraxreporterapi/repositories/AtBatRepository.java
+++ src/main/java/net/stlgamers/hittraxreporterapi/repositories/AtBatRepository.java
@@ -3,7 +3,6 @@ package net.stlgamers.hittraxreporterapi.repositories;
 import net.stlgamers.hittraxreporterapi.models.AtBat;
 import net.stlgamers.hittraxreporterapi.models.Session;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
